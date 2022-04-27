3064180e4e6a3aacf49dc7e51b4a28696232da92
JeanCharles14 <jeancduasan@gmail.com>
2022-04-26 21:05:21 -0300

    Implementing OAuth2 Autorization server

--- backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,21 +30,23 @@ import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundExceptions;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private BCryptPasswordEncoder encoder;
-	
+
 	@Autowired
 	private UserRepository repository;
 
 	@Autowired
 	private RoleRepository roleRepository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findallPaged(Pageable pageable) {
 		Page<User> list = repository.findAll(pageable);
-		return list.map(x -> new UserDTO(x));		
+		return list.map(x -> new UserDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -58,45 +65,52 @@ public class UserService {
 		return new UserDTO(entity);
 	}
 
-
 	@Transactional
 	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
-		User entity = repository.getOne(id);
-		copyDTOtoEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
-		}
-		catch(EntityNotFoundException e) {
-			throw new ResourceNotFoundExceptions("Id not found" + id); 
+			User entity = repository.getOne(id);
+			copyDTOtoEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundExceptions("Id not found" + id);
 		}
 	}
 
 	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
-		}
-		catch(EmptyResultDataAccessException e) {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundExceptions("Id not found" + id);
-		}
-		catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity Violation");
-			
+
 		}
 	}
-	
+
 	private void copyDTOtoEntity(UserDTO dto, User entity) {
-		
+
 		entity.setFirstName(dto.getFirstName());
 		entity.setLastName(dto.getLastName());
 		entity.setEmail(dto.getEmail());
-		
+
 		entity.getRoles().clear();
 		for (RoleDTO roleDTO : dto.getRoles()) {
 			Role role = roleRepository.getOne(roleDTO.getId());
 			entity.getRoles().add(role);
 		}
 	}
-	
-}
 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not Found: " + username);
+			throw new UsernameNotFoundException("Email not Found");
+		}
+		logger.info("User Found: " + username);
+		return user;
+	}
+
+}
